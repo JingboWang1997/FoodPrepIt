024573cf91571ffbb2f48cd56fb6a14ad9231775
JingboWang1997 <wjbjimmy@gmail.com>
2019-02-21 17:33:54 -0500

    button component, readme update

---
+++ frontend/src/components/fpitextbox_component.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Button } from 'reactstrap';
+
+class FPIButton extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+      return (
+        <div>
+          <Button variant="success">{this.props.text}</Button>
+        </div>
+      );
+  }
+}
+
+export default FPIButton;
