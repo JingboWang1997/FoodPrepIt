f277feb7ed499b94cea0495a73a88dbaf38b0685
Ruinan <nancywng@sina.com>
2019-02-28 13:07:39 -0500

    slider and tag

---
+++ frontend/src/components/fpitaginput_component.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { WithContext as ReactTags } from 'react-tag-input';
+
+
+
+class FPITaginput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tags: [{ id: 1, text: "Thailand" }, { id: 2, text: "India" }],
+    };
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleAddition = this.handleAddition.bind(this);
+    this.handleDrag = this.handleDrag.bind(this);
+    this.handleTagClick = this.handleTagClick.bind(this);
+  }
+
+  handleDelete(i) {
+    this.setState({
+      tags: this.state.tags.filter((tag, index) => index !== i),
+    });
+  }
+
+  handleAddition(tag) {
+    let { tags } = this.state;
+    this.setState({ tags: [...tags, { id: tags.length + 1, text: tag }] });
+  }
+
+  handleDrag(tag, currPos, newPos) {
+    const tags = [...this.state.tags];
+
+    // mutate array
+    tags.splice(currPos, 1);
+    tags.splice(newPos, 0, tag);
+
+    // re-render
+    this.setState({ tags });
+  }
+
+  handleTagClick(index) {
+    console.log('The tag at index ' + index + ' was clicked');
+  }
+
+  render() {
+    const { tags, suggestions } = this.state;
+    return (
+      <div>
+        <h4>ReactTags Demo</h4>
+        <ReactTags
+          tags={tags}
+          handleDelete={this.handleDelete}
+          handleAddition={this.handleAddition}
+          handleDrag={this.handleDrag}
+          handleTagClick={this.handleTagClick}
+        />
+      </div>
+    );
+  }
+}
+
+export default FPITaginput;
--- frontend/src/screens/components_screen.js
+++ frontend/src/screens/components_screen.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import FPIButton from '../components/fpibutton_component'
 import FPITabs from '../components/fpitabs_component'
+import FPISlider from '../components/fpislider_component'
+import FPITaginput from '../components/fpitaginput_component'
 
 class ComponentsScreen extends Component {
   render() {
     return (
       <div>
+        <FPISlider />
+        <FPITaginput />
         <h1>componenets</h1>
         <h3>button</h3>
         &lt;FPIButton text=&#123;&quot;text&quot;&#125;/&gt;
