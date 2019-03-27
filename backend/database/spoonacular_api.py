8b84d4d0c77dd88cd9d8e0cabd12e9bf7e3d0a21
Ruinan <nancywng@sina.com>
2019-03-27 12:01:01 -0400

    ingredient search yummly backend bug fix

--- backend/database/spoonacular_api.py
+++ backend/database/spoonacular_api.py
@@ -22,7 +22,7 @@ def getRecipe(id):
 
 
 def searchFromIngredients(ingredients):
-    baseURL = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=60&ranking=1&ingredients="
+    baseURL = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=40&ranking=1&ingredients="
     finalURL = baseURL + ingredients
     response = requests.get(finalURL,
         headers={"X-RapidAPI-Key": "e0908c685fmsh31f97109ebb2e50p1372a2jsncb26aea897b1"
--- backend/database/yummly_api.py
+++ backend/database/yummly_api.py
@@ -16,6 +16,8 @@ def getRecipe(id):
 def searchFromIngredients(ingredients):
     baseURL = "http://api.yummly.com/v1/api/recipes?_app_id=e5b85650&_app_key=ff1b1efa50ce7f1eed8e922aafca46a3&requirePictures=true"
     ingredientsList = ingredients.split(',')
+    ingredientsList = ingredientsList[:-1]
+    print(ingredientsList)
     for ing in ingredientsList:
         baseURL = baseURL + "&allowedIngredient[]=" + ing
     response = requests.get(baseURL)
